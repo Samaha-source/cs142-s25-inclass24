333e16bb66adf53a6018a67a7cf78cd60f780a48
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-04-10 23:13:29 -0500

    Polymorphism

---
+++ src/poly3/Shape.java
@@ -0,0 +1,18 @@
+package poly3;
+
+import java.awt.*;
+
+public abstract class Shape {
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double getArea();
+
+}
---
+++ src/poly3/Square.java
@@ -0,0 +1,14 @@
+package poly3;
+
+public class Square extends Shape {
+    private int sideLength;
+
+    public Square(int sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getArea()
+    {
+        return sideLength * sideLength;
+    }
+}
